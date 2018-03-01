7973565e0d8b37c176e39cd3b46f253c2e55b067
Alexa Scott <arscott4792@gmail.com>
2018-03-01 10:07:40 -0800

    updated html

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//initial page - loads
+//once user addds- adding:true
+//rating: null ??
+const store = {
+  bookmarks: [
+    { id: 1, title: 'Facebook', condensed: true}
+    { id: 2, title: 'YouTube', condensed: true}
+    { id: 3, title: 'Instagram', condensed: true}
+  ],
+  adding: false
+};
+
+
+ 
