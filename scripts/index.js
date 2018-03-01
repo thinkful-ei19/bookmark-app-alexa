ebe5933b9422f47801ff4928b6565f2c5ce1861b
Alexa Scott <arscott4792@gmail.com>
2018-03-01 12:20:50 -0800

    created function stubs and added api.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,80 @@
 // id ??
 const store = {
   bookmarks: [
-    { id: 1, title: 'Facebook', condensed: true, rating: null}
-    { id: 2, title: 'YouTube', condensed: true, rating: null}
+    { id: 1, title: 'Facebook', condensed: true, rating: null},
+    { id: 2, title: 'YouTube', condensed: true, rating: null},
     { id: 3, title: 'Instagram', condensed: true, rating: null}
   ],
   adding: false
 };
 
 //everytime render fxn runs (user does something) its looking at store to see if should add
+
+function generateBookmarkElement(bookmark, bookmarkIndex, template) {
+//returns html element as a string
+//dynamically - title
+}
+
+function generateBookmarkString(bookmarkList) {
+//returns a block of strings
+//will map over bookamrkList array and generateBookmarkElement on each item/index
+//return -- join
+}
+
+function renderBookmarkList() {
+    console.log('renderBookmarkList ran');
+}
+
+function handleAddButtonClicked() {
+    //event listener - listener for add button to be clicked
+    //target hidden class!
+    console.log('handleAddButtonClicked ran');
+
+}
+
+function addBookmarkToList(bookmarkName) {
+    //will push to store.bookmarks
+}
+
+// //function is for when user clicks add new bookmark
+// //.val...
+function handleNewBookmarkSubmit() {
+    //event listener 1. 2. 3.render
+    console.log('handleNewBookmarkSubmit ran');
+}
+
+
+function getBookmarkIndexFromElement() {
+
+}
+
+function handleRatingDropDownClicked() {
+    //event listener - listen for click on rating drop down
+    //will need function that...
+    console.log('handleRatingDropDownClicked ran');
+}
+
+
+function handleBookmarkDetailsClicked() {
+    console.log('handleBookmarkDetailsClicked ran');
+}
+
+function handleDeleteBookmarkClicked() {
+    console.log('handleDeleteBookmarkClicked ran');
+
+}
+
+
+function handleBookmarkList() {
+    renderBookmarkList();
+    handleAddButtonClicked();
+    handleNewBookmarkSubmit();
+    handleRatingDropDownClicked();
+    handleBookmarkDetailsClicked();
+    handleDeleteBookmarkClicked();
+  }
+  
+  $(handleBookmarkList);
 
 
  
